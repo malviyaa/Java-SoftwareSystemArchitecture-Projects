aaba343e6419da1a46905f3d087e630c81df1993
malviyaa <amalviy1@hawk.iit.edu>
2016-02-03 00:00:34 -0600

    second commit . Comment in main file

--- src/clockComponent/Main.java
+++ src/clockComponent/Main.java
@@ -7,7 +7,11 @@ public class Main {
 
 
 	public static void main(String[] args) {
-
+       /*
+        * 
+        * 
+        * my second commit
+        */
 		Timer times= new Timer(2,33,32,"PM","CST");
 		DigitalClock digital = new DigitalClock(times);
 		AnalogClock analog=new AnalogClock(times);
